2ec5190480560b12b79a625c11e968621520276c
Makaronodentro <gkontoa@gmail.com>
2015-11-30 23:09:03 +0000

    I hate vim

---
+++ QueuesAndDeques/src/QueuesDeques/Deque.java
@@ -0,0 +1,49 @@
+package QueuesDeques;
+
+/**
+ * Created by Makaronodentro on 30/11/2015.
+ */
+public class Deque<Item> implements Iterable<Item> {
+    // construct an empty deque
+    public Deque(){
+
+    }
+    // is the deque empty?
+    public boolean isEmpty(){
+
+    }
+    // return the number of items on the deque
+    public int size(){
+
+    }
+    // add the item to the front
+    public void addFirst(Item item){
+
+    }
+    // add the item to the end
+    public void addLast(Item item){
+
+    }
+    // remove and return the item from the front
+    public Item removeFirst(){
+
+    }
+    // remove and return the item from the end
+    public Item removeLast(){
+
+    }
+    // return an iterator over items in order from front to end
+    public Iterator<Item> iterator(){
+
+    }
+    // unit testing
+    public static void main(String[] args){
+
+    }
+}
+
+/*
+ * java.lang.UnsupportedOperationException if the client calls the remove() method in the iterator;
+ * throw a java.util.NoSuchElementException if the client calls the next() method in the iterator
+ * and there are no more items to return.
+ */
